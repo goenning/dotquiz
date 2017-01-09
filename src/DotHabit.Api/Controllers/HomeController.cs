eac5c4add87ce156d205e7a0118b63e01fabe3e2
goenning <me@goenning.net>
2017-01-09 21:48:28 +0000

    add env to controller

--- src/DotHabit.Api/Controllers/HomeController.cs
+++ src/DotHabit.Api/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotQuiz.Api.Controllers
@@ -6,11 +7,18 @@ namespace DotQuiz.Api.Controllers
     [Route("api")]
     public class HomeController : Controller 
     {
+        private IHostingEnvironment env;
+        public HomeController(IHostingEnvironment env)
+        {
+            this.env = env;
+        }
+
         [Route("values")]
         public IActionResult Index() 
         {
             return Ok(new {
                 now = DateTime.UtcNow,
+                env = env.EnvironmentName,
                 values = new[] {
                     Guid.NewGuid(),
                     Guid.NewGuid(),
