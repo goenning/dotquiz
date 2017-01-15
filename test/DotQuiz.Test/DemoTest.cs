7213f5ae4b80af9801fe28321d997630f7377a71
goenning <me@goenning.net>
2017-01-15 19:04:37 +0000

    unit test project setup

---
+++ test/DotQuiz.Test/DemoTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace DotQuiz.Test 
+{
+    public class DemoTest
+    {
+        [Fact]
+        public void Abc()
+        {
+            Assert.Equal(2, 2);
+        }
+    }
+}
